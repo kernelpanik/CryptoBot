7bfb759a4c283dd25b79a5072e3b5de2660af6ff
niKO <nicola.simionato@hotmail.it>
2022-05-13 16:17:27 +0200

    manage-coin.html

--- homepage/views.py
+++ homepage/views.py
@@ -35,7 +35,6 @@ class ManageCoinAddView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #user = self.request.user
         context["coinlist"] = CoinListAdd.object.all()
         return context
 
