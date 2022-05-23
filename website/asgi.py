1d46b04c5d3ec0a92acbc0116242b58446408535
niKO <nicola.simionato@hotmail.it>
2022-05-23 10:27:52 +0200

    daphne ok

--- website/asgi.py
+++ website/asgi.py
@@ -13,15 +13,15 @@ from django.core.asgi import get_asgi_application
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.conf.urls import url
+
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     # Just HTTP for now. (We can add other protocols later.)
-# })
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+})
 
 # # application = get_asgi_application()
 # # application = AmazingMiddleware(application)
