e36a1a9c6453393353aabc86a43e0f622e1771d8
niKO <nicola.simionato@hotmail.it>
2022-05-25 17:44:05 +0200

    working on channels

--- website/consumers.py
+++ website/consumers.py
@@ -1,17 +1,32 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class OhlcvConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
 
-    def disconnect(self, close_code):
-        pass
+# class OhlcvConsumer(WebsocketConsumer):
+#     def connect(self):
+#         self.accept()
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+#     def disconnect(self, close_code):
+#         pass
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+#     def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+
+#         self.send(text_data=json.dumps({
+#             'message': message
+#         }))
+
+class OhlcvConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.channel_layer.group_add('crypto', self.channel_name)
+        await self.accept()
+
+    async def disconnect(self):
+        await self.channel_layer.group_discard('crypto', self.channel_name)
+        await self.disconnect()
+
+    async def send_info(self, event):
+        msg = event['text']
+        await self.send(msg)        
