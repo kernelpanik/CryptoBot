2a2b12fef65422dcde5999fe30e421dcbc739274
niKO <nicola.simionato@hotmail.it>
2022-06-23 17:48:27 +0200

    sync

--- homepage/models.py
+++ homepage/models.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 
 
 class CoinList(models.Model):
-    date = models.DateTimeField(default=timezone.now)
+    date = models.DateTimeField(default=timezone.now, primary_key=True)
     coin = models.CharField(max_length=15)
     price = models.DecimalField(max_digits=36, decimal_places=18, null=True)
     totalbids = models.DecimalField(max_digits=36, decimal_places=18, null=True)
@@ -46,7 +46,7 @@ class Wallet(models.Model):
 
 
 class WalletAssetList(models.Model):
-    time = models.TimeField(auto_now=True)
+    time = models.TimeField(auto_now=True, primary_key=True)
     asset =  models.CharField(max_length=15)
     free = models.DecimalField(max_digits=36, decimal_places=18, default=0.0)
     locked = models.DecimalField(max_digits=36, decimal_places=18, default=0.0)
@@ -58,7 +58,7 @@ class WalletAssetList(models.Model):
 
 
 class WalletAssetBalance(models.Model):
-    time = models.DateTimeField(auto_now=True)
+    time = models.DateTimeField(auto_now=True, primary_key=True)
     usdtbalance = models.DecimalField(max_digits=19, decimal_places=8, default=0.0)
     btcbalance = models.DecimalField(max_digits=19, decimal_places=8, default=0.0)
 
@@ -74,7 +74,7 @@ class CryptoBotSettings(models.Model):
 
 class Ohlcv(models.Model):
     coin = models.CharField(max_length=15)
-    date = models.DateTimeField(default=timezone.now)
+    date = models.DateTimeField(default=timezone.now, primary_key=True)
     open = models.DecimalField(max_digits=36, decimal_places=18, null=True)
     high = models.DecimalField(max_digits=36, decimal_places=18, null=True)
     low = models.DecimalField(max_digits=36, decimal_places=18, null=True)
