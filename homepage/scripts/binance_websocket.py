6f7963d8146bb2fa1e75a4f21f6780a1a8946af5
niKO <nicola.simionato@hotmail.it>
2022-07-14 17:57:55 +0200

    sync

--- homepage/scripts/binance_websocket.py
+++ homepage/scripts/binance_websocket.py
@@ -4,6 +4,10 @@ import os
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 import asyncio
+from binance import AsyncClient, BinanceSocketManager
+from binance.enums import *
+
+
 
 
 
@@ -20,32 +24,45 @@ api_key = os.getenv("api_key")
 api_secret = os.getenv("api_secret")
 
 
-def websocket_kline(slug):
-    symbol = slug
-    twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
-    # start is required to initialise its internal loop
-    twm.start()
-    def handle_socket_message(msg):
-        print(f"message type: {msg['e']}")
-        print(msg)
-    twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
+# def websocket_kline(slug):
+#     symbol = slug
+#     twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
+#     # start is required to initialise its internal loop
+#     twm.start()
+#     def handle_socket_message(msg):
+#         print(f"message type: {msg['e']}")
+#         print(msg)
+#     twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
  
-    # # multiple sockets can be started
-    # twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
-    # twm.start_symbol_ticker_socket(callback=handle_socket_message, symbol=symbol)
+#     # # multiple sockets can be started
+#     # twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
+#     # twm.start_symbol_ticker_socket(callback=handle_socket_message, symbol=symbol)
 
 
-    # # or a multiplex socket can be started like this
-    # # see Binance docs for stream names
-    # streams = ['bnbusdt@ticker']
-    # twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
+#     # # or a multiplex socket can be started like this
+#     # # see Binance docs for stream names
+#     # streams = ['bnbusdt@ticker']
+#     # twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
 
-    twm.join()
-    websocket_kline()
-
-if __name__ == "__websocket_kline__":
-   websocket_kline()
+#     twm.join()
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(websocket_kline(slug))
 
 
 
-    
+
+
+async def websocket_kline(slug):
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client)
+    # start any sockets here, i.e a trade socket
+    ts = bm.kline_socket('BNBBTC', interval=KLINE_INTERVAL_30MINUTE)
+    print(ts)
+    # then start receiving messages
+    async with ts as tscm:
+        while True:
+            res = await tscm.recv()
+            print(res)
+
+
+  
