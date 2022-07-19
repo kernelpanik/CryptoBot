608a5b5a7f27f23a7e3dca6c08e7d1692d84c2bc
niKO <nicola.simionato@hotmail.it>
2022-07-19 16:25:31 +0200

    sync

--- website/consumers.py
+++ website/consumers.py
@@ -3,16 +3,13 @@ from channels.generic.websocket import AsyncWebsocketConsumer, async_to_sync
 
 
 class OhlcvConsumer(AsyncWebsocketConsumer):
-
-    async def ws_connect(self):
+    async def connect(self):
         await self.accept()
         await self.send(text_data="connected")
         await self.channel_layer.group_add("crypto_ohlcv", self.channel_name)
         print(f"Added {self.channel_name} channel to crypto_ohlcv")
 
-        
-
-    async def ws_disconnect(self, close_code):
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard("crypto_ohlcv", self.channel_name)
         print(f"Removed {self.channel_name} channel to crypto_ohlcv")
 
