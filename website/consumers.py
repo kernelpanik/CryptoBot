c1d7491794bbf63fd0dedcc79414d7d66b278b98
niKO <nicola.simionato@hotmail.it>
2022-07-22 11:06:01 +0200

    sync

--- website/consumers.py
+++ website/consumers.py
@@ -7,7 +7,6 @@ class OhlcvConsumer(AsyncWebsocketConsumer):
         await self.accept()
         await self.send(text_data="connected")
         await self.channel_layer.group_add("crypto_ohlcv", self.channel_name)
-#        await self.channel_layer.group_add("crypto_ohlcv", "test")
 
         print(f"Added {self.channel_name} channel to crypto_ohlcv")
 
