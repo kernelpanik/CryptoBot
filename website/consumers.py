98b5e39db435177fef296162cdea3b9bee2f0589
niKO <nicola.simionato@hotmail.it>
2022-07-22 14:09:11 +0200

    sync

--- website/consumers.py
+++ website/consumers.py
@@ -1,14 +1,31 @@
 from channels.generic.websocket import WebsocketConsumer
 from channels.generic.websocket import AsyncWebsocketConsumer, async_to_sync
+from time import sleep
+import json
+import asyncio
+
 
 
 class OhlcvConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-        await self.send(text_data="connected")
+#        await self.send(text_data="connected")
         await self.channel_layer.group_add("crypto_ohlcv", self.channel_name)
 
         print(f"Added {self.channel_name} channel to crypto_ohlcv")
+
+        for i in range(10):
+            await self.send(json.dumps({
+                "type": "websocket.accept",
+                # "send": {'text_data': str(i)}
+                'text_data':str(i)
+            }))
+            # sleep(1)
+            await asyncio.sleep(1)
+
+            # await self.send(text_data=str(i))
+
+
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard("crypto_ohlcv", self.channel_name)
